9b97229774a6c6bf6a092b695314bb76c6e7dab5
CesarXotlanihua <122295327+CesarXotlanihua@users.noreply.github.com>
2023-01-19 18:15:13 -0600

    Add files via upload

---
+++ Cuadradoorectángulo.java
@@ -0,0 +1,29 @@
+package Actividad2;
+import java.util.*;
+public class Main {
+	public static void Main(String[] args) {
+		int base=10;
+		int altura=15;
+		int area=0;
+		int diagonal=0;
+		perimetro=0;
+		numero=(base*base)+(altura*altura);
+		double diagonal = Math.sqrt(numero);
+		System.out.println(resultado);
+		int area=base*altura;
+		System.out.println(area);
+		perimetro=base+base+altura+altura;
+		System.out.println(perimetro);
+	}
+public class Circulo{
+	public static void Main(String[] args) {
+		float areac=0;
+		float circunferencia=0;
+		int radio=10;
+		areac =3.14*(radio*radio);
+		circunferencia=3.14*(radio+radio);
+		System.out.println("El area es:"areac);
+		System.out.println("La circunferencia es:"circunferencia);
+	}
+}
+}
